44a337b4ffd84a67017117015a5ab58cdbd88eb3
Prince <53673962+asemgyimah@users.noreply.github.com>
2019-10-21 10:26:47 -0700

    Add files via upload

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'acebc78aa9339bb9ac6d';
+    this.client_secret = '49641fffc8b747a649b19f436bc1874fe6fab3a8';
+    this.repos_count = 7;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
